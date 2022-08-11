5ff3be04d3a5ad0c43300c5de44bf5b420b48d47
Fernando <ferg.developer@gmail.com>
2022-08-11 14:23:58 -0400

    library app continuation

--- Assignments/1.LibraryApp/app.js
+++ Assignments/1.LibraryApp/app.js
@@ -1,83 +1,86 @@
-
+// user interface
 const librarySection = document.querySelector('.book-container')
-
-let library = [
-    {
-        id : 1,
-        author: 'Fer',
-        num_pages : 189,
-        title: 'First Book',
-        read : false
-    },
-    {
-        id : 2,
-        author: 'Fernando Gonzalez',
-        num_pages : 989,
-        title: 'Second Book',
-        read : true
-    },
-    {
-        id : 3,
-        author: 'Jhon Gonzalez',
-        num_pages : 989,
-        title: 'Third Book',
-        read : true
-    }
-];
+const addBookForm = document.querySelector('#addBookForm')
 
 
-function Book(id, author, num_pages, title, read) {
+class Book {
     // constructor
-    this.id = id
-    this.author = author
-    this.num_pages = num_pages
-    this.title = title
-    this.read = read
+    constructor(author, num_pages, title, isRead) {
+        this.author = author
+        this.num_pages = num_pages
+        this.title = title
+        this.isRead = isRead
+    }
 }
 
-function addFunctionToLibrary() {
-    // do stuff here
-    addBook()
+class Library {
+    constructor() {
+        this.books = []
+    }
 
+    addBook(newBook) {
+        this.books.push(newBook)
+    }
 }
 
+const library = new Library()
 
-function displayBook(books){
-    books.map((book)=>{
-        const card = document.createElement('div')
-        card.classList.add('card');
-
-        const bookTitle = document.createElement('h1');
-        bookTitle.classList.add('book-title')
-        const bookAuthor = document.createElement('h3');
-        bookAuthor.classList.add('book-author');
-        const bookPages = document.createElement('span');
-        bookPages.classList.add('book-pages');
-        const bookRead = document.createElement('span');
-        bookRead.classList.add('book-read');
-        const bookId = document.createElement('span');
-        bookId.classList.add('book-id');
-
-        const bookIdText = document.createTextNode(book.id); 
-        const bookTitleText = document.createTextNode(book.title); 
-        const bookAuthorText = document.createTextNode(book.author); 
-        const bookPagesText = document.createTextNode(book.num_pages); 
-        const bookReadText = document.createTextNode(book.read); 
-        
-        bookId.appendChild(bookIdText);
-        bookTitle.appendChild(bookTitleText);
-        bookAuthor.appendChild(bookAuthorText);
-        bookPages.appendChild(bookPagesText);
-        bookRead.appendChild(bookReadText);
-        
-        librarySection.append(card)
-
-        card.appendChild(bookId);
-        card.appendChild(bookTitle);
-        card.appendChild(bookAuthor);
-        card.appendChild(bookPages);
-        card.appendChild(bookRead);
-    })
+const updateBooksGrid = () => {
+    resetBooksGrid()
+    for (let book of library.books) {
+        createBookCard(book)
+    }
 }
 
-displayBook(library)
+const resetBooksGrid = () => {
+    librarySection.innerHTML = '';
+}
+
+const createBook = (book) => {
+    const card = document.createElement('div')
+    const bookTitle = document.createElement('h1');
+    const bookAuthor = document.createElement('h3');
+    const bookPages = document.createElement('span');
+    const bookRead = document.createElement('span');
+
+    card.classList.add('card');
+    bookTitle.classList.add('book-title');
+    bookAuthor.classList.add('book-author');
+    bookPages.classList.add('book-pages');
+    bookRead.classList.add('book-read');
+
+    // const bookTitleText = document.createTextNode(book.title);
+    // const bookAuthorText = document.createTextNode(book.author);
+    // const bookPagesText = document.createTextNode(book.num_pages);
+    // const bookReadText = document.createTextNode(book.read);
+
+    // bookTitle.appendChild(bookTitleText);
+    // bookAuthor.appendChild(bookAuthorText);
+    // bookPages.appendChild(bookPagesText);
+    // bookRead.appendChild(bookReadText);
+
+    librarySection.append(card)
+
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookRead);
+};
+
+
+
+function getInputValues() {
+    const author = document.querySelector('#author')
+    console.log("Author: ", author.addEventListener('change', (e)=>{
+        console.log(e.target.value)
+    }));
+    const num_pages = document.querySelector('#pages').value
+    const title = document.querySelector('#title').value
+    const read = document.querySelector('#read').checked
+    return new Book(author, num_pages, title, read)
+};
+
+const addBook = (e) => {
+    e.preventDefault()
+    const newBook = getInputValues()
+}
