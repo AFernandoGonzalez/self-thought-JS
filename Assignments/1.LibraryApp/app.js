5c3c6d8cab134ea9c0210b515fd9e219fb714cf3
Fernando <ferg.developer@gmail.com>
2022-08-11 20:04:51 -0400

    modal added to book form

--- Assignments/1.LibraryApp/app.js
+++ Assignments/1.LibraryApp/app.js
@@ -3,6 +3,32 @@ const librarySection = document.querySelector('.book-container')
 const addBookForm = document.querySelector('#addBookForm')
 const errorMsg = document.querySelector('#errorMsg')
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//     modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 class Book {
     // constructor
     constructor(author, num_pages, title, isRead) {
@@ -40,6 +66,10 @@ const updateBooksGrid = () => {
 const resetBooksGrid = () => {
     librarySection.innerHTML = '';
 }
+
+const closeAddBookModal = () => {
+    modal.style.display = "none";
+  }
 
 const createBook = (book) => {
     const card = document.createElement('div')
@@ -96,7 +126,11 @@ const addBook = (e) => {
     library.addBook(newBook)
     updateBooksGrid()
 
+    closeAddBookModal()
+    
     console.log("library.books: ", library.books);
 }
 
 addBookForm.onsubmit = addBook
+
+
